93f60024e8a7c2ab6f13b23329596e3b13141f42
c4explosive <ao946391@gmail.com>
2015-06-29 03:58:24 -0500

    Listo.

--- PROY3_1.C
+++ PROY3_1.C
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <conio.h>
 #include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+/*Integrantes:
+Angel Espinosa 8-905-1352
+Mar¡a T. Rodr¡guez 8-900-1207
+Eduardo Perez [CED]
+*/
+void nntol(char npal[],float num );
 
 /////////////////////DATOS////////////////////
 char nombre[10];
@@ -40,7 +49,7 @@ void main()
     gotoxy(53,9);printf("SEC.");
     gotoxy(60,9);printf("EMPL.");
     gotoxy(70,9);printf("CATG.");
-    gotoxy(25,15);printf("*************REGULAR*************");
+    gotoxy(25,15);printf("**************************");
     gotoxy(2,17);printf("S. BRUTO");
     gotoxy(15,17);printf("S. SOCIAL");
     gotoxy(30,17);printf("ISR");
@@ -57,7 +66,7 @@ void main()
 
 void datos()
 {
-
+    char npal[500]="";
     nche++;
     gotoxy(69,7);
     printf("%d",nche);
@@ -83,6 +92,11 @@ void datos()
     scanf("%d",&empl);
     gotoxy(70,10);
     scanf("%d",&catg);
+    switch(catg)
+    {
+	case 1: gotoxy(25,15); printf("***********REGULAR***************");break;
+	case 2: gotoxy(25,15); printf("***********AGREGADO**************");
+    }
     gotoxy(2,18);
     scanf("%f",&bruto);
     gotoxy(15,18);
@@ -96,5 +110,137 @@ void datos()
     sueldo = (bruto)-(s_social+isr+seduc+fcps);
     gotoxy(65,18);
     printf("%.2f",sueldo);
+    nntol(npal,sueldo);
+    gotoxy(10,22);printf("*************************%s******************",npal);
+    gotoxy(10,23);printf("*************************************************************");
+
+}
+void nntol(char npal[],float num )
+{
+    char mun[5];
+    char armun[5];
+    char spos[5];
+    char dtext[30];
+    //char npal[500]="";
+    char uni[30][30]={"\0","UNO","DOS","TRES","CUATRO",
+		   "CINCO","SEIS","SIETE","OCHO","NUEVE","DIEZ","ONCE","DOCE",
+		   "TRECE","CATORCE","QUINCE","DICISIES","DIECISIETE","DIECIOCHO",
+		   "DIECINUEVE","VEINTE","VIENTIUNO","VEINTIDOS","VEINTITRES","VEINTICUATRO",
+		   "VEITICINCO","VEINTISEIS","VEINTISIETE","VEINTIOCHO","VEINTINUEVE"};
+    char decc[10][30]={"\0","\0","\0","TREINTA","CUARENTA","CINCUENTA","SESENTA","SETENTA","OCHENTA",
+			"NOVENTA"};
+    char cent[30][30]={"CIEN","CIENTO","DOSCIENTOS","TRESCIENTOS","CUATROCIENTOS","QUINIENTOS",
+			"SEISCIENTOS","SETECIENTOS","OCHOCIENTOS","NOVECIENTOS"};
+    double nnmu,dnum,ddec;
+    int i,k=0,j,pos,upos;
+    //printf("%.0f",floor(num));
+    sprintf(mun,"%.0f",floor(num));
+    dnum=nnmu=atoi(mun);
+    j=strlen(mun)-1;
+    for(i=4;i>=0;i--)
+    {
+	if(j>=0)
+	{
+	    armun[i]=mun[j];
+	    j--;
+	}
+	else
+	    armun[i]='0';
+    }
+    if(armun[0]=='0' && armun[1]=='0' && armun[2]=='1' && armun[3]=='0' && armun[4]=='0')
+	strcat(npal,cent[0]);
+    else
+    {
+    for(i=0;i<5;i++)
+    {
+	switch(i)
+	{
+	     case 0:
+		 if(armun[i]!='0' || armun[i+1]!='0')
+		 {
+		 for(j=0;j<2;j++)
+		 {
+		     spos[j]=armun[i+k];
+		     k++;
+		 }
+		 pos=atoi(spos);
+		 //printf("POS: %d",pos);
+		 if (pos<29)
+		 {
+		     if(pos!=1)
+			strcat(npal,uni[pos]);
+		     strcat(npal,"MIL ");
+		 }
+		 else
+		 {
+		     pos=armun[i]-0x30;
+		     upos=armun[i+1]-0x30;
+		     if(upos != 0)
+		     {
+			strcat(npal,decc[pos]);
+			strcat(npal," Y ");
+		     }
+		     else
+		     {
+			strcat(npal,decc[pos]);
+		     }
+		     strcat(npal,uni[upos]);
+		     strcat(npal," MIL ");
+		 }
+		 }
+		 break;
+	     case 2:
+		 if(armun[i] != '0')
+		 {
+		 pos=armun[i]-0x30;
+		 //printf("%d",pos);
+		 strcat(npal,cent[pos]);
+		 strcat(npal," ");
+		 //printf("%s",cent[pos]);
+		 }
+		 break;
+	     case 3:
+		 if(armun[i] != '0'|| armun[i+1]!=0)
+		 {
+		 for(j=0;j<2;j++)
+		 {
+		     spos[j]=armun[i+k];
+		     k++;
+		 }
+		 pos=atoi(spos);
+		 //printf("POS: %d",pos);
+		 if (pos<29)
+		 {
+		     strcat(npal,uni[pos]);
+		     strcat(npal," ");
+		 }
+		 else
+		 {
+		     pos=armun[i]-0x30;
+		     upos=armun[i+1]-0x30;
+		     if(upos!=0)
+		     {
+			strcat(npal,decc[pos]);
+			strcat(npal," Y ");
+		     }
+		     else
+			strcat(npal,decc[pos]);
+		     strcat(npal,uni[upos]);
+		     strcat(npal," ");
+		 }
+		 }
+		 break;
+	}
+	k=0;
+    }
+    }
+    ddec=num-dnum;
+    if(ddec==0)
+	sprintf(dtext,"CON 0%.0f/100",ddec*100);
+    else
+	sprintf(dtext,"CON %.0f/100",ddec*100);
+    strcat(npal,dtext);
+    //printf("%s",npal);
+    //getch();
 
 }
